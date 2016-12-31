8891c9642f0ad4f7d2e63012500bb24119ecdbf5
Lucas McDaniel <ldmcdaniel@gmail.com>
2016-12-31 09:32:57 -0600

    Mostly working example

--- app.js
+++ app.js
@@ -1,6 +1,57 @@
 new Vue({
   el: '#events',
-  data: {},
-  ready: function () {},
-  methods: {}
+
+  data: {
+    event: {
+      name: '',
+      description: '',
+      date: ''
+    },
+    events: []
+  },
+
+  ready: function () {
+    this.fetchEvents();
+  },
+
+  methods: {
+    fetchEvents: function () {
+      var events = [
+        {
+          id: 1,
+          name: 'Toronto International Film Festival',
+          date: '2015-09-10'
+        },
+        {
+          id: 2,
+          name: 'The Martian Premiere',
+          description: 'The Martian comes to theateres.',
+          date: '2015-10-02'
+        },
+        {
+          id: 3,
+          name: 'SXSW',
+          description: 'Music, film and interactive festival in Austin, TX',
+          date: '2016-03-11'
+        }
+      ];
+
+      this.$set('events', events);
+    },
+    addEvent: function () {
+      if (this.event.name) {
+        this.events.push(this.event);
+        this.event = {
+          name: '',
+          description: '',
+          date: ''
+        };
+      }
+    },
+    deleteEvent: function (index) {
+      if (confirm('Are you sure you want to delete this event?')) {
+        this.events.$remove(index);
+      }
+    }
+  }
 });
